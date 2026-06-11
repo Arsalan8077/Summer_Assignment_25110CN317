5bb1f7ef80ca578818df934a9eebbc72bdcc8869
Mohd Arsalan <mohdarsalanktpr@gmail.com>
2026-06-11 12:17:17 +0530

    added Day-8 programes

---
+++ Day-8/programe30.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int rows,i,j;// deceleration of variables 
+    printf("enter the number of rows");
+    scanf("%d",&rows);
+    for (i=1;i<=rows;i++)// loop for couloumn
+    {
+    for(j=1;j<=i;j++)// loop for rows
+    {
+    printf("%d",j);
+    }
+    printf("\n");
+    }
+return 0;
+}
